594f759e43c2f17f9dde3167120c01ff66a34af9
Roni <ronikumar34645@gmail.com>
2021-05-06 08:09:30 +0530

    implemented stripe checkout

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
+const bookingCtrl = require('./controllers/bookingController');
 const viewRouter = require('./routes/viewRoutes');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -56,6 +57,13 @@ const limiter = rateLimit({
 });
 
 app.use('/api', limiter);
+
+// ============== Stripe Checkout ===============
+app.post(
+  '/webhook-checkout',
+  express.raw({ type: 'application/json' }),
+  bookingCtrl.webhookCheckout
+);
 
 // Body parser
 app.use(express.json({ limit: '10kb' }));
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,15 +1,9 @@
 const express = require('express');
 const viewsCtrl = require('../controllers/viewsController');
 const authCtrl = require('../controllers/authController');
-const bookingCtrl = require('../controllers/bookingController');
 const router = express.Router();
 
-router.get(
-  '/',
-  authCtrl.isLoggedIn,
-  bookingCtrl.createBookingCheckout,
-  viewsCtrl.getOverview
-);
+router.get('/', authCtrl.isLoggedIn, viewsCtrl.getOverview);
 router.get('/tour/:slug', authCtrl.isLoggedIn, viewsCtrl.getTour);
 router.get('/login', authCtrl.isLoggedIn, viewsCtrl.getLoginForm);
 router.get('/signup', authCtrl.isLoggedIn, viewsCtrl.getSignupForm);
